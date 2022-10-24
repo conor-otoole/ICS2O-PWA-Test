f7f578b59ab467e0b5ae8ae871bc664ce26e0046
conor-otoole <113055588+conor-otoole@users.noreply.github.com>
2022-10-24 14:42:28 -0400

    fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: conor-otoole
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
